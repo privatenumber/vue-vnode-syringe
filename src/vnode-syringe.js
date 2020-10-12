5971f4433347d7658484dc13502366202eecc82f
Hiroki Osame <hiroki.osame@gmail.com>
2020-10-12 01:37:29 -0400

    wip

--- src/vnode-syringe.js
+++ src/vnode-syringe.js
@@ -1,113 +1,12 @@
+
+
 const hyphenateRE = /\B([A-Z])/g;
 const hyphenate = string => string.replace(hyphenateRE, '-$1').toLowerCase();
 
-const {hasOwnProperty} = Object.prototype;
-const hasOwn = (object, key) => hasOwnProperty.call(object, key);
-
-// From https://github.com/vuejs/vue/blob/6fe07eb/src/platforms/web/util/style.js#L5
-const parseStyleText = cssText => {
-	const res = {};
-	const listDelimiter = /;(?![^(]*\))/g;
-	const propertyDelimiter = /:(.+)/;
-	cssText.split(listDelimiter).forEach(item => {
-		if (item) {
-			const temporary = item.split(propertyDelimiter);
-			temporary.length > 1 && (res[temporary[0].trim()] = temporary[1].trim());
-		}
-	});
-	return res;
-};
-
-function normalizeModifiers(object, handlers) {
-	for (const key in object) {
-		if (!hasOwn(object, key)) {
-			continue;
-		}
-
-		const modifier = key[key.length - 1];
-		if (modifier === '&' || modifier === '!') {
-			const strippedKey = key.slice(0, -1);
-
-			const handler = handlers && handlers[strippedKey];
-			if (handler) {
-				handler(object[key], modifier);
-			} else {
-				object[strippedKey] = {
-					value: object[key],
-					modifier
-				};
-			}
-
-			delete object[key];
-		} else {
-			object[key] = {
-				value: object[key],
-			};
-		}
-	}
-}
-
-const set = (object, attr, {modifier, value}) => {
-	// Overwrite
-	if (modifier === '!' || !hasOwn(object, attr)) {
-		object[attr] = value;
-		return;
-	}
-
-	const base = object[attr];
-	if (modifier === '&' && base) {
-		if (attr === 'class') {
-			object[attr] = [base, value];
-			return;
-		}
-
-		if (attr === 'staticClass') {
-			object[attr] += ` ${value}`;
-			return;
-		}
-
-		if (Array.isArray(base)) {
-			if (Array.isArray(value)) {
-				base.push.apply(base, value);
-			} else {
-				base.push(value);
-			}
-
-			return;
-		}
-
-		if (typeof base === 'object') {
-			Object.assign(base, value);
-			return;
-		}
-
-		if (typeof base === 'function' && typeof value === 'function') {
-			object[attr] = function () {
-				Reflect.apply(base, this, arguments);
-				Reflect.apply(value, this, arguments);
-			};
-		}
-	}
-};
-
-const assign = (target, object) => {
-	for (const attr in object) {
-		if (hasOwn(object, attr)) {
-			set(target, attr, object[attr]);
-		}
-	}
-
-	return target;
-};
-
-const classStyleProps = {
-	staticClass: 'staticClass',
-	class: 'class',
-	staticStyle: 'staticStyle',
-	style: 'style',
-};
-
-const classStyles = Object.values(classStyleProps);
+const camelizeRE = /-(\w)/g;
+const camelize = (function (str) {
+	return str.replace(camelizeRE, function (_, c) { return c ? c.toUpperCase() : ''; })
+});
 
 const isEmpty = (obj) => {
 	for (const key in obj) {
@@ -115,97 +14,124 @@ const isEmpty = (obj) => {
 	}
 	return true;
 }
+
+const isNil = val => val === undefined || val === null;
+
+const mergeStatic = (data, name) => {
+	const staticProp = 'static' + name[0].toUpperCase() + name.slice(1);
+	if (!data[name] && !data[staticProp]) {
+		return;
+	}
+	data[name] = [
+		data[staticProp] ? data[staticProp] : [],
+		data[name] ? data[name] : [],
+	].flat(Infinity);
+	delete data[staticProp];
+};
+
+const { hasOwnProperty } = Object.prototype;
+
+/*
+Needs to handle
+attrs: {
+	a: 1,
+	b: 2,
+	c: 3
+}
+*/
+function merge(dest, fromObj) {
+	if (isNil(fromObj)) {
+		return dest;
+	}
+
+	if (isNil(dest)) {
+		return /*Array.isArray(fromObj) ? fromObj.slice(0) :*/ fromObj;
+	}
+
+	else if (!Array.isArray(dest) && typeof dest === 'object') {
+		return Object.assign({}, fromObj, dest);
+	}
+
+	return dest;
+}
+
+
+function findProp(propDefs, attr) {
+	if (hasOwnProperty.call(propDefs, attr)) {
+		return attr;
+	}
+
+	const kebab = hyphenate(attr);
+	if (hasOwnProperty.call(propDefs, kebab)) {
+		return kebab;
+	}
+
+	const camel = camelize(attr);
+	if (hasOwnProperty.call(propDefs, camel)) {
+		return camel;
+	}
+
+	return false;
+}
+
+function getPropsData(componentOptions, attrs) {
+	const { props } = componentOptions.Ctor.options;
+	const propsData = {};
+
+	// Iterate over given attrs
+	for (const attr in attrs) {
+
+		// Check if the attr is a prop
+		const isProp = findProp(props, attr);
+		if (isProp) {
+			propsData[isProp] = attrs[attr];
+			attrs[attr] = undefined;
+		}
+	}
+	return propsData;
+}
+
 const vnodeSyringe = {
 	functional: true,
 	render(h, {children, data}) {
-		if (!children) {
-			return;
-		}
-
-		if (isEmpty(data)) {
+		if (!children || isEmpty(data)) {
 			return children;
 		}
 
-		classStyles.forEach(prop => {
-			if (data[prop]) {
-				data[prop] = {
-					value: data[prop],
-				};
-			}
-		});
-
-		normalizeModifiers(data.attrs, {
-			style(value, modifier) {
-				let prop = 'style';
-				if (typeof value === 'string') {
-					prop = 'staticStyle';
-					value = parseStyleText(value);
-				}
-
-				data[prop] = {
-					value: value,
-					modifier: modifier,
-				};
-			},
-			class(value, modifier) {
-				const prop = typeof value === 'string' ? 'staticClass' : 'class';
-				data[prop] = {
-					value: value,
-					modifier: modifier,
-				};
-			},
-		});
-
-		normalizeModifiers(data.on);
+		mergeStatic(data, 'class');
+		mergeStatic(data, 'style');
 
 		return children.map(vnode => {
 			if (vnode.tag) {
-				const d = Object.create(data);
-				const {componentOptions: comOptions} = vnode;
-
-				if (comOptions) {
-					const propOptions = comOptions.Ctor.options.props;
-					if (propOptions) {
-						d.attrs = Object.assign({}, d.attrs);
-
-						for (const prop in propOptions) {
-							if (!hasOwn(propOptions, prop)) {
-								continue;
-							}
-
-							[prop, hyphenate(prop)].some(propp => {
-								for (const key in d.attrs) {
-									if (hasOwn(d.attrs, key) && key === propp) {
-										set(comOptions.propsData, prop, d.attrs[key]);
-										delete d.attrs[key];
-										return true;
-									}
-								}
-
-								return false;
-							});
-						}
-					}
-
-					// Logic from https://github.com/vuejs/vue/blob/v2.6.11/src/core/vdom/create-component.js#L168
-					comOptions.listeners = assign(comOptions.listeners || {}, d.on);
-					d.on = undefined;
-				}
-
 				if (!vnode.data) {
 					vnode.data = {};
 				}
+				const {data: vnodeData} = vnode;
 
-				classStyles.forEach(prop => {
-					if (d[prop]) {
-						set(vnode.data, prop, d[prop]);
-					}
-				});
+				mergeStatic(vnodeData, 'class');
+				mergeStatic(vnodeData, 'style');
 
-				['attrs', 'on'].forEach(prop => {
-					if (d[prop]) {
-						vnode.data[prop] = assign(vnode.data[prop] || {}, d[prop]);
-					}
+				const attrs = { ...data.attrs };
+				const on = { ...data.on };
+				const nativeOn = { ...data.nativeOn };
+
+				// If component
+				const {componentOptions} = vnode;
+				if (componentOptions) {
+					const propsData = getPropsData(componentOptions, attrs);
+					componentOptions.propsData = merge(componentOptions.propsData, propsData);
+					componentOptions.listeners = merge(componentOptions.listeners, on);
+
+					vnodeData.nativeOn = merge(vnodeData.nativeOn, nativeOn);
+					vnodeData.on = vnodeData.nativeOn;
+				} else {
+					vnodeData.on = merge(vnodeData.on, on);
+				}
+				vnodeData.attrs = merge(vnodeData.attrs, attrs);
+
+				vnode.data = merge(vnodeData, {
+					class: data.class,
+					style: data.style,
 				});
 			}
 
--- test/fallback.test.js
+++ test/fallback.test.js
@@ -1,64 +1,490 @@
 import {mount} from '@vue/test-utils';
 import VnodeSyringe from 'vue-vnode-syringe';
 
+describe('fallback', () => {
 
-describe('fallback attrs', () => {
+	describe('native element', () => {
+		test('apply new attributes', () => {
+			const wrapper = mount({
+				template: `
+					<vnode-syringe
+						class="static-class"
+						:class="['dynamic-class', 2, ['3', 2]]"
+						style="color:red"
+						:style="{ font: 'serif' }"
+						a="1"
+						:b="2"
+						:c="'3'"
+					>
+						<div/>
+					</vnode-syringe>
+				`,
+				components: {
+					VnodeSyringe,
+				},
+			});
+			expect(wrapper.html()).toBe('<div a="1" b="2" c="3" class="static-class dynamic-class 3" style="color: red; font: serif;"></div>');
+		});
 
-	test('Apply attributes to single child', () => {
-		const usage = {
-			template: `
-				<vnode-syringe
-					class="static-class"
-					:class="'dynamic-class'"
-					style="color:red"
-					:style="'font:serif'"
-					a="1"
-					:b="2"
-					:c="'3'"
-				>
-					<div />
-				</vnode-syringe>
-			`,
-			components: {
-				VnodeSyringe,
-			},
-		};
+		test('preserve existing attributes', () => {
+			const wrapper = mount({
+				template: `
+					<vnode-syringe
+						class="static-class"
+						:class="'dynamic-class'"
+						style="color:red"
+						:style="'font:serif'"
+						a="1"
+						:b="2"
+						:c="'3'"
+					>
+						<div
+							class="some-class"
+							style="color:blue"
+							a="3"
+							b="1"
+							c="2"
+						/>
+					</vnode-syringe>
+				`,
+				components: {
+					VnodeSyringe,
+				},
+			});
+			expect(wrapper.html()).toBe('<div a="3" b="1" c="2" class="some-class" style="color: blue;"></div>');
+		});
 
-		const wrapper = mount(usage);
-		expect(wrapper.html()).toBe('<div a="1" b="2" c="3" class="static-class dynamic-class" style="color: red; font: serif;"></div>');
+		test('multiple elements', () => {
+			const wrapper = mount({
+				template: `
+					<div>
+						<vnode-syringe
+							class="static-class"
+							:class="'dynamic-class'"
+							style="color:red"
+							:style="'font:serif'"
+							a="1"
+							:b="2"
+							:c="'3'"
+						>
+							<div
+								class="some-class"
+							/>
+							<div
+								:class="'some-class'"
+							/>
+							<div
+								style="color:blue"
+							/>
+							<div
+								:style="{color:'blue'}"
+							/>
+							<div
+								b="c"
+							/>
+						</vnode-syringe>
+					</div>
+				`,
+				components: {
+					VnodeSyringe,
+				},
+			});
+			expect(wrapper.html()).toBe(`<div>\n  <div a="1" b="2" c="3" class="some-class" style="color: red;"></div>\n  <div a="1" b="2" c="3" class="some-class" style="color: red;"></div>\n  <div a="1" b="2" c="3" class="static-class dynamic-class" style="color: blue;"></div>\n  <div a="1" b="2" c="3" class="static-class dynamic-class" style="color: blue;"></div>\n  <div a="1" b="c" c="3" class="static-class dynamic-class" style="color: red;"></div>\n</div>`);
+		});
+
+		describe('event-handlers', () => {
+			test('apply new @handler', async () => {
+				const onClick = jest.fn();
+				const onFocus = jest.fn();
+				const wrapper = mount({
+					template: `
+						<vnode-syringe
+							@click="onClick"
+							@focus="onFocus"
+						>
+							<div/>
+						</vnode-syringe>
+					`,
+					components: {
+						VnodeSyringe,
+					},
+					methods: {
+						onClick,
+						onFocus,
+					},
+				});
+				await wrapper.trigger('click');
+				expect(onClick).toHaveBeenCalled();
+				await wrapper.trigger('focus');
+				expect(onFocus).toHaveBeenCalled();
+			});
+
+			test('preserve existing @handlers', async () => {
+				const onClick = jest.fn();
+				const onFocus = jest.fn();
+				const wrapper = mount({
+					template: `
+						<vnode-syringe
+							@click="onClick"
+							@focus="onFocus"
+						>
+							<div @click=""/>
+						</vnode-syringe>
+					`,
+					components: {
+						VnodeSyringe,
+					},
+					methods: {
+						onClick,
+						onFocus,
+					},
+				});
+				await wrapper.trigger('click');
+				expect(onClick).not.toHaveBeenCalled();
+				await wrapper.trigger('focus');
+				expect(onFocus).toHaveBeenCalled();
+			});
+
+			// v-on.native is not allowed on native elements
+
+			test('multiple elements', async () => {
+				const onClick = jest.fn();
+
+				const wrapper = mount({
+					template: `
+						<div>
+							<vnode-syringe
+								class="static-class"
+								:class="'dynamic-class'"
+								style="color:red"
+								:style="'font:serif'"
+								a="1"
+								:b="2"
+								:c="'3'"
+								@click="onClick"
+							>
+								<div
+									class="some-class"
+									@click=""
+								/>
+								<div
+									:class="'some-class'"
+								/>
+								<div
+									style="color:blue"
+									@click=""
+								/>
+								<div
+									:style="{color:'blue'}"
+								/>
+								<div
+									b="c"
+								/>
+							</vnode-syringe>
+						</div>
+					`,
+					components: {
+						VnodeSyringe,
+					},
+					methods: {
+						onClick,
+					},
+				});
+
+				const divs = wrapper.findAll('div');
+				await divs.trigger('click');
+				expect(onClick).toHaveBeenCalledTimes(3);
+			});
+		});
 	});
 
-	test('Apply attributes to single child', () => {
-		const usage = {
-			template: `
-				<vnode-syringe
-					class="static-class"
-					:class="'dynamic-class'"
-					style="color:red"
-					:style="'font:serif'"
-					a="1"
-					:b="2"
-					:c="'3'"
-				>
-					<div
-						class="some-class"
-						style="color:blue"
-						a="3"
-						b="1"
-						c="2"
-					/>
-				</vnode-syringe>
-			`,
-			components: {
-				VnodeSyringe,
-			},
-		};
+	describe('component', () => {
+		let TestComp;
 
-		const wrapper = mount(usage);
-		expect(wrapper.html()).toBe('<div a="3" b="1" c="2" class="some-class" style="color: blue;"></div>');
+		beforeEach(() => {
+			TestComp = {
+				props: ['camelCase', 'kebab-case', 'PascalCase', 'snake_case', 'UPPER_CASE'],
+				template: '<div v-on="$listeners">{{ camelCase }} {{ kebabCase }} {{ PascalCase }} {{ snake_case }} {{ UPPER_CASE }}</div>',
+			};
+		});
+
+		test('only apply new attributes/props', () => {
+			const wrapper = mount({
+				template: `
+					<vnode-syringe
+						class="static-class"
+						:class="['dynamic-class', 2, ['3', 2]]"
+						style="color:red"
+						:style="{ font: 'serif' }"
+						camelCase="-"
+						kebab-case="-"
+						PascalCase="-"
+						snake_case="-"
+						UPPER_CASE="-"
+						random-attr="123"
+					>
+						<test-comp
+							class="some-class"
+							camelCase="camelCase"
+							kebab-case="kebab-case"
+							PascalCase="PascalCase"
+							snake_case="snake_case"
+						/>
+					</vnode-syringe>
+				`,
+				components: {
+					VnodeSyringe,
+					TestComp,
+				},
+			});
+			expect(wrapper.html()).toBe('<div random-attr="123" class="some-class" style="color: red; font: serif;">camelCase kebab-case PascalCase snake_case -</div>');
+		});
+
+		test('dont apply if inheritAttrs: false', () => {
+			const NoInheritAttrs = {
+				...TestComp,
+				inheritAttrs: false,
+			};
+
+			const wrapper = mount({
+				template: `
+					<vnode-syringe
+						class="static-class"
+						:class="['dynamic-class', 2, ['3', 2]]"
+						style="color:red"
+						:style="{ font: 'serif' }"
+						a="1"
+						:b="2"
+						:c="'3'"
+					>
+						<no-inherit-attrs/>
+					</vnode-syringe>
+				`,
+				components: {
+					VnodeSyringe,
+					NoInheritAttrs,
+				},
+			});
+			expect(wrapper.html()).toBe('<div class="static-class dynamic-class 3" style="color: red; font: serif;"> </div>');
+		});
+
+		describe('event-handlers', () => {
+			test('dont apply without v-on="$listeners"', async () => {
+				const NoInheritListeners = {
+					...TestComp,
+					template: '<div></div>',
+				};
+
+				const onClick = jest.fn();
+				const onFocus = jest.fn();
+
+				const wrapper = mount({
+					template: `
+						<vnode-syringe
+							@click="onClick"
+							@focus="onFocus"
+						>
+							<no-inherit-listeners />
+						</vnode-syringe>
+					`,
+					components: {
+						VnodeSyringe,
+						NoInheritListeners,
+					},
+					methods: {
+						onClick,
+						onFocus,
+					},
+				});
+				await wrapper.trigger('click');
+				expect(onClick).not.toHaveBeenCalled();
+				await wrapper.trigger('focus');
+				expect(onFocus).not.toHaveBeenCalled();
+			});
+
+			test('apply new @handler', async () => {
+				const onClick = jest.fn();
+				const onFocus = jest.fn();
+
+				const wrapper = mount({
+					template: `
+						<vnode-syringe
+							@click="onClick"
+							@focus="onFocus"
+						>
+							<test-comp />
+						</vnode-syringe>
+					`,
+					components: {
+						VnodeSyringe,
+						TestComp,
+					},
+					methods: {
+						onClick,
+						onFocus,
+					},
+				});
+				await wrapper.trigger('click');
+				expect(onClick).toHaveBeenCalled();
+				await wrapper.trigger('focus');
+				expect(onFocus).toHaveBeenCalled();
+			});
+
+			test('preserve existing @handler', async () => {
+				const onClick = jest.fn();
+				const onFocus = jest.fn();
+
+				const wrapper = mount({
+					template: `
+						<vnode-syringe
+							@click="onClick"
+							@focus="onFocus"
+						>
+							<test-comp @click=""/>
+						</vnode-syringe>
+					`,
+					components: {
+						VnodeSyringe,
+						TestComp,
+					},
+					methods: {
+						onClick,
+						onFocus,
+					},
+				});
+				await wrapper.trigger('click');
+				expect(onClick).not.toHaveBeenCalled();
+				await wrapper.trigger('focus');
+				expect(onFocus).toHaveBeenCalled();
+			});
+
+			test('apply new @handler.native', async () => {
+				const NoInheritListeners = {
+					...TestComp,
+					template: '<div></div>',
+				};
+
+				const onClick = jest.fn();
+				const onFocus = jest.fn();
+
+				const wrapper = mount({
+					template: `
+						<vnode-syringe
+							@click.native="onClick"
+							@focus.native="onFocus"
+						>
+							<no-inherit-listeners />
+						</vnode-syringe>
+					`,
+					components: {
+						VnodeSyringe,
+						NoInheritListeners,
+					},
+					methods: {
+						onClick,
+						onFocus,
+					},
+				});
+				await wrapper.trigger('click');
+				expect(onClick).toHaveBeenCalled();
+				await wrapper.trigger('focus');
+				expect(onFocus).toHaveBeenCalled();
+			});
+
+			test('preserve existing @handler.native', async () => {
+				const NoInheritListeners = {
+					...TestComp,
+					template: '<div></div>',
+				};
+
+				const onClick = jest.fn();
+				const onFocus = jest.fn();
+
+				const wrapper = mount({
+					template: `
+						<vnode-syringe
+							@click.native="onClick"
+							@focus.native="onFocus"
+						>
+							<no-inherit-listeners @click.native=""/>
+						</vnode-syringe>
+					`,
+					components: {
+						VnodeSyringe,
+						NoInheritListeners,
+					},
+					methods: {
+						onClick,
+						onFocus,
+					},
+				});
+				await wrapper.trigger('click');
+				expect(onClick).not.toHaveBeenCalled();
+				await wrapper.trigger('focus');
+				expect(onFocus).toHaveBeenCalled();
+			});
+
+			test('multiple elements', async () => {
+				const NoInheritListeners = {
+					...TestComp,
+					template: '<div></div>',
+				};
+				const onClick = jest.fn();
+				const onNativeClick = jest.fn();
+
+				const wrapper = mount({
+					template: `
+						<div>
+							<vnode-syringe
+								class="static-class"
+								:class="'dynamic-class'"
+								style="color:red"
+								:style="'font:serif'"
+								a="1"
+								:b="2"
+								:c="'3'"
+								@click="onClick"
+								@click.native="onNativeClick"
+							>
+								<no-inherit-listeners
+									class="some-class"
+									@click=""
+								/>
+								<no-inherit-listeners
+									:class="'some-class'"
+								/>
+								<div
+									style="color:blue"
+								/>
+								<no-inherit-listeners
+									:style="{color:'blue'}"
+								/>
+								<test-comp
+									b="c"
+								/>
+								<test-comp
+									b="c"
+									@click
+								/>
+							</vnode-syringe>
+						</div>
+					`,
+					components: {
+						VnodeSyringe,
+						TestComp,
+						NoInheritListeners,
+					},
+					methods: {
+						onClick,
+						onNativeClick,
+					},
+				});
+
+				const divs = wrapper.findAll('div');
+				await divs.trigger('click');
+				expect(onClick).toHaveBeenCalledTimes(2);
+				expect(onNativeClick).toHaveBeenCalledTimes(5);
+			});			
+		});
 	});
-
-
-
-
 });
