2e35739c518fcb5c7cb11c9779f1b30d8f7589b1
Hiroki Osame <hiroki.osame@gmail.com>
2020-10-14 01:32:53 -0400

    wip

---
+++ src/utils.js
@@ -0,0 +1,153 @@
+const FALLBACK = 0;
+const OVERWRITE = 1;
+const MERGE = 2;
+const MODIFIERS = {
+	'!': OVERWRITE,
+	'&': MERGE,
+};
+
+const hyphenateRE = /\B([A-Z])/g;
+const camelizeRE = /-(\w)/g;
+
+function hyphenate(string) {
+	return string.replace(hyphenateRE, '-$1').toLowerCase();
+}
+
+function camelize(string) {
+	return string.replace(camelizeRE, (_, c) => c ? c.toUpperCase() : '');
+}
+
+export function isEmpty(object) {
+	// eslint-disable-next-line no-unreachable-loop
+	for (const key in object) {
+		return false;
+	}
+
+	return true;
+}
+
+export function merge(dest, src) {
+	for (const key in src) {
+		if (src[key]) {
+			const {value, modifier} = src[key];
+			const destValue = dest[key];
+			if (
+				destValue === undefined ||
+				destValue === null ||
+				modifier === OVERWRITE
+			) {
+				dest[key] = value;
+			} else if (modifier === MERGE) {
+				if (Array.isArray(destValue)) {
+					// eslint-disable-next-line max-depth
+					if (Array.isArray(value)) {
+						destValue.push(...value);
+					} else {
+						destValue.push(value);
+					}
+				} else if (typeof destValue === 'object' && typeof value === 'object') {
+					Object.assign(destValue, value);
+				} else if (typeof destValue === 'function' && typeof value === 'function') {
+					dest[key] = function () {
+						Reflect.apply(destValue, this, arguments);
+						Reflect.apply(value, this, arguments);
+					};
+				} else {
+					dest[key] += value;
+				}
+			}
+		}
+	}
+
+	return dest;
+}
+
+function findProp(attrs, prop) {
+	if (prop in attrs) {
+		return prop;
+	}
+
+	const kebab = hyphenate(prop);
+	if (kebab in attrs) {
+		return kebab;
+	}
+
+	return false;
+}
+
+export function getPropsData(componentOptions, attrs) {
+	const {props} = componentOptions.Ctor.options;
+	const propsData = {};
+
+	if (props) {
+		// Iterate over props to find attrs that are props
+		// By iterating over props instead of attrs, we can leverage Vue's camelization
+		for (const prop in props) {
+			const isProp = findProp(attrs, prop);
+			if (isProp) {
+				propsData[prop] = attrs[isProp];
+				delete attrs[isProp];
+			}
+		}
+	}
+
+	return propsData;
+}
+
+export function parseModifiers(object) {
+	const normalized = {};
+	for (let key in object) {
+		const value = object[key];
+		let modifier = MODIFIERS[key.slice(-1)];
+		if (modifier) {
+			key = key.slice(0, -1);
+		} else {
+			modifier = FALLBACK;
+		}
+
+		normalized[key] = {value, modifier};
+	}
+
+	return normalized;
+}
+
+export function getStaticPair(object, name) {
+	const staticProp = camelize('static-' + name);
+	const pair = [object[staticProp], object[name]].filter(Boolean).flat(Infinity);
+	if (pair.length === 0) {
+		return undefined;
+	}
+
+	delete object[staticProp];
+	return pair;
+}
+
+export function getStyle(name, attrs, data) {
+	const value_ = attrs[name];
+	if (value_) {
+		delete attrs[name];
+		return value_;
+	}
+
+	const pair = getStaticPair(data, name);
+	if (!pair) {
+		return undefined;
+	}
+
+	return {
+		value: pair,
+		modifier: FALLBACK,
+	};
+}
+
+export function parseStyles(styleString) {
+	return Object.fromEntries(
+		styleString
+			.split(';')
+			.map(pair => {
+				const [property, value] = pair.split(':');
+				return (property && value) && [camelize(property.trim()), value.trim()];
+			})
+			.filter(Boolean),
+	);
+}
